a4fe3894a382037038317c5acc87833aa370ea1d
ngyikai <ykng0508@gmail.com>
2024-06-27 20:19:47 +0800

    updated layout

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -8,7 +8,6 @@ import githubLight from '../../assets/github-light.svg';
 import githubDark from '../../assets/github-dark.svg';
 import linkedinLight from '../../assets/linkedin-light.svg';
 import linkedinDark from '../../assets/linkedin-dark.svg';
-import CV from '../../assets/cv.pdf';
 import { useTheme } from '../../common/ThemeContext';
 
 function Hero() {
@@ -50,9 +49,9 @@ function Hero() {
         <p className={styles.description}>
           With a passion for developing modern software and apps.
         </p>
-        <a href={CV} download>
+        {/* <a href={CV} download>
           <button className="hover">Resume</button>
-        </a>
+        </a> */}
       </div>
     </section>
   );
