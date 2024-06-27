b20d1874f02e11f864f937767175686dcf2893b8
NgYiKai <62179279+NgYiKai@users.noreply.github.com>
2024-06-27 08:55:06 +0000

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import './App.css';
+import Contact from './sections/Contact/Contact';
+import Footer from './sections/Footer/Footer';
+import Projects from './sections/Projects/Projects';
+import Skills from './sections/Skills/Skills';
+import Hero from './sections/Hero/Hero';
+
+
+function App() {
+  return (
+    <>
+      <Hero />
+      <Projects />
+      <Skills />
+      <Contact />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
